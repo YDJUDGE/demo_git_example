3ec663dd4c8070c61fa9f2bf008077627c9bec0f
Denis Vyalov <denis123456k@yandex.ru>
2024-09-30 19:05:40 +0300

    demo zip

--- 14th_lesson_git/main.py
+++ 14th_lesson_git/main.py
@@ -1,16 +1,8 @@
 
-def demo_zip():
-    values1 = list(range(2, 10))
-    values2 = list(range(3, 19, 3))
-    print(values1)
-    print(values2)
-    for v1, v2 in zip(values1, values2):
-        print("v1 = ", v1, "v2 = ", v2, "sum = ", v1 + v2)
-
 def main():
-    # print("Hello_main")
-    demo_zip()
+    print("Hello_main")
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
 
